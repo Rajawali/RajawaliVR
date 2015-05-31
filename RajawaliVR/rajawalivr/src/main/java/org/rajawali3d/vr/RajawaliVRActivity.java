546d5cfbbde8aeaf973c30138b1799d548190512
Dennis Ippel <dennis.ippel@rehabstudio.com>
2015-05-31 11:36:54 +0100

    Upgraded to the lates Cardboard SDK

--- RajawaliVR/rajawalivr/src/main/java/org/rajawali3d/vr/RajawaliVRActivity.java
+++ RajawaliVR/rajawalivr/src/main/java/org/rajawali3d/vr/RajawaliVRActivity.java
@@ -2,11 +2,16 @@ package org.rajawali3d.vr;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
+import com.google.vrtoolkit.cardboard.sensors.DeviceSensorLooper;
 import com.google.vrtoolkit.cardboard.sensors.HeadTracker;
+import com.google.vrtoolkit.cardboard.sensors.SystemClock;
 
 import org.rajawali3d.surface.IRajawaliSurface;
 import org.rajawali3d.surface.RajawaliSurfaceView;
@@ -32,7 +37,13 @@ public class RajawaliVRActivity extends Activity {
 	            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
 	            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
 	            | View.SYSTEM_UI_FLAG_IMMERSIVE);
-		mHeadTracker = new HeadTracker(this);
+
+
+        SensorManager sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        Display display = ((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+
+        mHeadTracker = new HeadTracker(new DeviceSensorLooper(sensorManager), new SystemClock(), display);
 	}
 	
 	protected void setRenderer(RajawaliVRRenderer renderer) {
